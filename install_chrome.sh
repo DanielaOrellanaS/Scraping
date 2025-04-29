d0d334a8300a2e29027de17ff1325fa29d0129e1
DanielaOrellanaS <orellanadaniela1111@gmail.com>
2025-04-29 11:21:49 -0500

    fix the path with chromedriver

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+# Instala dependencias necesarias
 apt-get update && apt-get install -y wget unzip curl gnupg2 ca-certificates
 
-# Instala Google Chrome estable
+# Instala Google Chrome estable (versión compatible con chromedriver 135)
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb || true
 
@@ -15,4 +16,9 @@ mkdir -p bin
 CHROMEDRIVER_VERSION=135.0.7049.114
 wget https://storage.googleapis.com/chrome-for-testing-public/$CHROMEDRIVER_VERSION/linux64/chromedriver-linux64.zip
 unzip -o chromedriver-linux64.zip -d bin
+
+# Mueve chromedriver a la carpeta bin
+mv bin/chromedriver-linux64/chromedriver bin/
+
+# Da permisos de ejecución al archivo
 chmod +x bin/chromedriver
